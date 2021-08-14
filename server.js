7fbfc52e69aecfbe0a3cb55aafde31c085384381
Jeongjjuna <wlgns2234@naver.com>
2021-08-15 00:45:38 +0900

    QnA1 data input

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({extended: true}));
 
 
 app.listen(3000, function(요청, 응답){
@@ -10,6 +12,7 @@ app.listen(3000, function(요청, 응답){
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
+app.use(express.urlencoded({extended: true}))
 
 //-------------------------------------------------------------------------
 app.get("/mainpage",(request,response) => {
@@ -26,7 +29,12 @@ app.get("/page3",(request,response) => {
     response.render('page3.ejs')
   });
 
-
   app.get("/QnA_1",(request,response) => {
     response.render('QnA_1.ejs')
   });
+
+//-------------------------------------------------------------------------
+app.post('/add', function(request,response){
+  response.send('전송완료');
+  console.log(request.body)
+});
