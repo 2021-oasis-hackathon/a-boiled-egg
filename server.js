be4138701b1db1ccae308c7ccd7524d1b1ccc709
Jeongjjuna <wlgns2234@naver.com>
2021-08-15 01:34:10 +0900

    data response server

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(express.urlencoded({extended: true}));
+const MongoClient = require('mongodb').MongoClient;
 
-
-app.listen(3000, function(요청, 응답){
+var db;
+MongoClient.connect('mongodb+srv://hackathon1234:qwer1234@cluster0.9xmuh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(에러, client){
+  if (에러) return console.log(에러);
+  db = client.db('todoapp');
+  app.listen(3000, function(request, response){
     console.log('서버실행!');
+  });
 })
+
 
 //-------------------------------------------------------------------------
 
@@ -18,7 +24,6 @@ app.use(express.urlencoded({extended: true}))
 app.get("/mainpage",(request,response) => {
   response.render('mainpage.ejs')
 });
-
 app.get("/page1",(request,response) => {
     response.render('page1.ejs')
   });
@@ -28,13 +33,14 @@ app.get("/page2",(request,response) => {
 app.get("/page3",(request,response) => {
     response.render('page3.ejs')
   });
-
   app.get("/QnA_1",(request,response) => {
     response.render('QnA_1.ejs')
   });
-
 //-------------------------------------------------------------------------
 app.post('/add', function(request,response){
   response.send('전송완료');
-  console.log(request.body)
-});
+  console.log(request.body);
+  db.collection('hackathon').insertOne({내용전체 : 요청.body}, (에러,) =>{
+    console.log('저장완료!');
+  });
+});  
