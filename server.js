e8db1e00055d992a1d30be782889ed9e414d1bf1
Jeongjjuna <wlgns2234@naver.com>
2021-08-15 03:17:36 +0900

    QnA2,3,4,5 make

--- server.js
+++ server.js
@@ -36,15 +36,39 @@ app.get("/page3",(request,response) => {
   app.get("/QnA_1",(request,response) => {
     response.render('QnA_1.ejs')
   });
+
+  app.get("/QnA_2",(request,response) => {
+    response.render('QnA_2.ejs')
+  });
+
+  app.get("/QnA_3",(request,response) => {
+    response.render('QnA_3.ejs')
+  });
+
+  app.get("/QnA_4",(request,response) => {
+    response.render('QnA_4.ejs')
+  });
+
+  app.get("/QnA_5",(request,response) => {
+    response.render('QnA_5.ejs')
+  });
 //-------------------------------------------------------------------------
 app.post('/add', function(request,response){
   response.send('전송완료');
   console.log(request.body);
-  console.log(request.body.contents[1])
   for ( let i=0 ; i<3 ; i++ ){
-    db.collection('collection').insertOne( { 내용: request.body.contents[i]} , function(에러,결과){
+    db.collection('collection').insertOne( { _id : i, 내용: request.body.contents[i]} , function(에러,결과){
       console.log('저장완료!');
     });
   }
-  
 });
+
+
+
+//-------------------------------------------------------------------------몽고db 모든 데이터 가져오기
+app.get('/list', function(요청, 응답){
+  db.collection('collection').find().toArray(function(에러, 결과){
+    console.log(결과)
+    응답.render('QnAlist.ejs', { collections : 결과 })
+  })
+})
