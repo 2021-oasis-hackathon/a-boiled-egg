38364e4c497be14e036fd095b4873a8e1a1810ee
Jeongjjuna <wlgns2234@naver.com>
2021-08-18 12:29:01 +0900

    loginsesstion

--- server.js
+++ server.js
@@ -30,9 +30,13 @@ app.get("/mainpage",(request,response) => {
   response.render('mainpage.ejs')
 });
 
+app.get("/page1",(request,response) => {
+  response.render('page1.ejs')
+});
+
 app.get("/page2",(request,response) => {
     response.render('page2.ejs')
-  });
+  }); 
 app.get("/page3",(request,response) => {
     response.render('page3.ejs')
   });
@@ -40,15 +44,15 @@ app.get("/page3",(request,response) => {
 
 
 //-------------------------------------------------------------------------저장하기
-app.post('/add', function(request,response){
-  response.send('전송완료');
-  console.log(request.body);
-  for ( let i=0 ; i<3 ; i++ ){
-    db.collection('collection').insertOne( { _id : i, 내용: request.body.contents[i]} , function(에러,결과){
-      console.log('저장완료!');
-    });
-  }
-});
+// app.post('/add', function(request,response){
+//   response.send('전송완료');
+//   console.log(request.body);
+//   for ( let i=0 ; i<3 ; i++ ){
+//     db.collection('collection').insertOne( { _id : i, 내용: request.body.contents[i]} , function(에러,결과){
+//       console.log('저장완료!');
+//     });
+//   }
+// });
 
 
 
@@ -62,7 +66,7 @@ app.get('/list', function(요청, 응답){
 })
 
 
-//----------------------------------------------------------------------------로그인기능
+//----------------------------------------------------------------------------로그인기능세팅
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
@@ -70,7 +74,36 @@ const session = require('express-session');
 app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.get("/page1",(request,response) => {
-  response.render('page1.ejs')
+//-----------------------------------------------------------------------------로그인포스트요청실행
+app.post('/add', passport.authenticate('local', {
+  failureRedirect : '/fail'
+}), function(req, res){
+  res.redirect('/mainpage')
 });
+
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (input_id, input_pw, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: input_id }, function (error, result) {
+    if (error) return done(error)
+
+    if (!result) return done(null, false, { message: '존재하지않는 아이디입니다' })
+    if (input_pw == result.pw) {
+      return done(null, result)
+    } else {
+      return done(null, false, { message: '비미번호가 틀렸어요' })
+    }
+  })
+}));
+//-----------------------------------------------로그인세션만들기
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+});
+
+passport.deserializeUser(function (아이디, done) {
+  done(null, {})
+}); 
